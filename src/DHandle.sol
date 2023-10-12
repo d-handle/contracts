816d2765ac5a4394c07881a7e8bd6c46450ad248
prc <prcolaco@gmail.com>
2023-10-12 03:18:22 +0100

    added helpers and checkers for handles

--- src/DHandle.sol
+++ src/DHandle.sol
@@ -7,10 +7,13 @@ import "./SoulBoundERC721.sol";
 /// @dev Implements the staking and auction mechanisms of the DHandle protocol
 contract DHandle is SoulBoundERC721 {
 
-    // @dev Time in seconds to wait for a bid to close and be able to claim ownership of a handle
+    /// @notice Error thrown when an invalid handle is passed
+    error InvalidHandle(string handle); 
+
+    /// @dev Time in seconds to wait for a bid to close and be able to claim ownership of a handle
     uint256 private constant BID_CLOSE = 30 days;
 
-    /// @info An handle is the tokenId, as uint256 representation of a /[a-z0-9_-]{3,32}/ handle string
+    /// @info A handle is the tokenId, as uint256 representation of a /[a-z0-9_-]{3,32}/ handle string
 
     /// @dev Staked amounts for each handle
     mapping(uint256 handle => uint256) internal _stakeOf;
@@ -30,7 +33,9 @@ contract DHandle is SoulBoundERC721 {
     /// @dev Initializes the contract by setting ERC721 `name` and `symbol`
     constructor() ERC721("DHandle", "DHandle") { }
 
-    // function mint() {}
+    function mint(string memory handle, string memory uri) external {
+        
+    }
 
     // function update() {}
 
@@ -46,8 +51,43 @@ contract DHandle is SoulBoundERC721 {
 
 
     /// @dev resolve a handle to the IPFS link of its JSON metadata
-    function tokenURI(uint256 handle) public view override returns (string memory) {
-        _requireOwned(handle);
-        return _uriOf[handle];
+    function resolve(string memory handle) external view returns (string memory) {
+        return tokenURI(toTokenId(handle));
+    }
+
+    /// @dev return the IPFS link of token id JSON metadata
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        _requireOwned(tokenId);
+        return _uriOf[tokenId];
+    }
+
+    /// @dev Helper function to check and convert from handle string to token id number
+    function toTokenId(string memory handle) pure public returns (uint256) {
+        if (checkHandle(handle) == false) revert InvalidHandle(handle);
+        return uint256(toBytes32(handle));
+    }
+
+    function checkHandle(string memory handle) pure public returns (bool) {
+        bool lastSpecial = false;
+        bytes32  b32 = toBytes32(handle);
+        for (uint256 i = 0; i < 32; i++) {
+            uint8 c = uint8(b32[i]);
+            if (i == 0 && (c == 0x2d || c == 0x5f)) return false;   // start with special
+            if (c == 0 && lastSpecial) return false;                // end with special
+            if (c == 0 && i > 2) return true;                       // end of handle with at least 3 valid chars
+            if (c < 0x30 && c != 0x2d) return false;                // abaixo do '0' e diferente de '-'
+            if (c > 0x39 && c < 0x61 && c != 0x5f) return false;    // acima do '9' e abaixo do 'a' e diferente do '_'
+            if (c > 0x7a) return false;                             // acima do 'z'
+            lastSpecial = (c == 0x2d || c == 0x5f);                 // is it a special char ('-' or '_')
+        }
+        return true;
+    }
+
+    function toBytes32(string memory handle) pure internal returns (bytes32 result) {
+        bytes memory temp = bytes(handle);
+        if (temp.length == 0 || temp.length > 32) return 0;
+        assembly {
+            result := mload(add(handle, 32))
+        }
     }
 }
