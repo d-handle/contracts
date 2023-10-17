1a696a67f1ba01823c3edf3304a06e305c89a1b5
prc <prcolaco@gmail.com>
2023-10-17 02:01:49 +0100

    add auction check functions and started bid

--- src/DHandle.sol
+++ src/DHandle.sol
@@ -5,20 +5,25 @@ import "./SoulBoundERC721.sol";
 
 
 // Custom errors
-error InvalidHandle(string handle);                     // An invalid handle is passed
-error StakeAmountRequired();                            // No stake amount is sent
-error HandleNotAvailable(string handle);                // The handle is already registered
-error InvalidFrontendAddress();                         // Frontend address is 0x0
-error FailedToSendFee(address frontend, uint256 fee);   // Error transfering fee to frontend 
-error NotHandleOwner(string handle);                    // Only handle owner is allowed
-error NotEnoughStake(string handle, uint256 amount);    // Not enough stake to request
+error InvalidHandle(string handle);                         // An invalid handle is passed
+error EthAmountRequired();                                  // No eth amount is sent
+error InvalidEthAmount(uint256 required, uint256 amount);   // Not the required eth amount
+error HandleNotAvailable(string handle);                    // The handle is already registered
+error HandleNotRegistered(string handle);                   // The handle is not registered
+error InvalidToAddress();                                   // To address is 0x0
+error InvalidFrontendAddress();                             // Frontend address is 0x0
+error FailedToTransfer(address to, uint256 amount);         // Error transfering fee to frontend
+error NotHandleOwner(string handle);                        // Only handle owner is allowed
 
 /// @title DHandle NFT contract
 /// @dev Implements the staking and auction mechanisms of the DHandle protocol
 contract DHandle is SoulBoundERC721 {
 
-    /// @dev Time in seconds to wait for a bid to close and be able to claim ownership of a handle
-    uint256 private constant BID_CLOSE = 30 days;
+    /// @dev Time in seconds to wait for an auction to close and be able to claim ownership of a handle
+    uint256 private constant AUCTION_WINDOW = 30 days;
+
+    /// @dev Time in seconds to claim ownership of a handle after auction closed
+    uint256 private constant CLAIM_WINDOW = 3 days;
 
 
     // INFO: A handle is the tokenId, as uint256 representation of a /[a-z0-9_-]{3,32}/ handle string
@@ -36,39 +41,30 @@ contract DHandle is SoulBoundERC721 {
     /// @dev Current higher bid amount for the tokenIds in auction
     mapping(uint256 tokenId => uint256) internal _bidAmountOf;
 
-    /// @dev Current higher bid closing timestamps for the tokenIds in auction
-    mapping(uint256 tokenId => uint256) internal _bidCloseOf;
+    /// @dev Current higher bid timestamps for the tokenIds in auction
+    mapping(uint256 tokenId => uint256) internal _bidTimeOf;
 
 
     /// @dev Initializes the contract by setting ERC721 `name` and `symbol`
     constructor() ERC721("DHandle", "DHandle") { }
 
 
-    /// @notice register a new handle if available, by staking any starting anount to it and set the initial profile uri
-    function mint(string memory handle, string memory uri) external payable {
-        mint(handle, uri, msg.value, address(0));
+    /// @notice register a new handle if available, by staking any starting amount to it and set the initial profile uri
+    function mint(address to, string memory handle, string memory uri) external payable {
+        mint(to, handle, uri, msg.value, address(0));
     }
 
-    /// @notice register a new handle if available, by staking any starting anount to it and set the initial profile uri, with frontend fees
-    function mint(string memory handle, string memory uri, uint256 stake, address frontend) public payable {
-        if (msg.value == 0 || stake == 0) revert StakeAmountRequired();
-        if (msg.value < stake) revert NotEnoughStake(handle, stake);
-        if (frontend == address(0)) revert InvalidFrontendAddress();
+    /// @notice register a new handle if available, by staking any starting amount to it and set the initial profile uri, with frontend fees
+    function mint(address to, string memory handle, string memory uri, uint256 amount, address frontend) public payable {
+        if (to == address(0)) revert InvalidToAddress();
 
         uint256 id = toTokenId(handle);
         if (_ownerOf(id) != address(0)) revert HandleNotAvailable(handle);
 
-        _stakeOf[id] += stake;
+        _safeMint(to, id);
         _uriOf[id] = uri;
 
-        _safeMint(msg.sender, id);
-
-        // Send frontend fee if any
-        uint256 fee = msg.value - stake;
-        if (fee > 0) {
-            (bool sent, ) = frontend.call{value: fee}("");
-            if (!sent) revert FailedToSendFee(frontend, fee);
-        }
+        _deposit(id, amount, frontend);
     }
 
     /// @notice update the profile uri of the handle by the owner only
@@ -77,31 +73,132 @@ contract DHandle is SoulBoundERC721 {
         _uriOf[id] = uri;
     }
 
-    // function bid() {}
-
-    // function claim() {}
-
-    // function deposit() {}
-
-    function withdraw(string memory handle, uint256 amount) public {
-        uint256 id = _requireOwner(handle);
-        _withdraw(handle, id, amount);
+    /// @notice place a bid in a handle
+    function bid(string memory handle) external payable {
+        bid(handle, msg.value, address(0));
     }
 
-    function _withdraw(string memory handle, uint256 tokenId, uint256 amount) internal {
-        if (amount > _stakeOf[tokenId]) revert NotEnoughStake(handle, amount);
-        // TODO: transfer and update stake
+    /// @notice place a bid in a handle, with frontend fees
+    function bid(string memory handle, uint256 amount, address frontend) public payable {
+        // TODO: during claim window no bids accepted
+        uint256 id = _requireOwner(handle);
+        if (msg.value == 0 || amount == 0) revert EthAmountRequired();
+        uint256 bidValue = _stakeOrBidOf(id) * 2;
+        if (amount != bidValue) revert InvalidEthAmount(bidValue, amount);
+        if (msg.value < amount) revert InvalidEthAmount(amount, msg.value);
+        if (frontend == address(0) && msg.value != amount) revert InvalidFrontendAddress();
+        // TODO: refund current bid, if any, and place new bid
+    }
+
+    /// @notice cancel a bid by the bidder, if after the auction closed the full bid amount will be returned, otherwise only the proportional full days passed
+    /// of the auction window will be returned to the bidder
+    // function retract() {}
+
+    /// @notice claim the handle from current owner if auction closed and return current owner stake
+    // function claim() {}
+
+    /// @notice returns the current staked amount (or higher bid if any) for this handle
+    function stakeOf(string memory handle) public view returns (uint256 stake) {
+        uint256 id = toTokenId(handle);
+        stake = _stakeOrBidOf(id);
+    }
+
+    /// @dev internal to return the current staked amount, or higher bid if any, for this token id
+    function _stakeOrBidOf(uint256 tokenId) internal view returns (uint256 stake) {
+        stake = _isBidValid(tokenId) ? _bidAmountOf[tokenId] : _stakeOf[tokenId];
+    }
+
+    /// @notice returns if the current bid is still valid
+    function isBidValid(string memory handle) external view returns (bool) {
+        uint256 id = toTokenId(handle);
+        return _isBidValid(id);
+    }
+
+    /// @dev internal to check if current bid is still valid
+    function _isBidValid(uint256 tokenId) internal view returns (bool) {
+        return _bidderOf[tokenId] != address(0) && block.timestamp <= (_bidTimeOf[tokenId] + AUCTION_WINDOW + CLAIM_WINDOW);
+    }
+
+    /// @notice returns if the current auction is still open
+    function isAuctionOpen(string memory handle) external view returns (bool) {
+        uint256 id = toTokenId(handle);
+        return _isAuctionOpen(id);
+    }
+
+    /// @dev internal to check if current auction is still open
+    function _isAuctionOpen(uint256 tokenId) internal view returns (bool) {
+        return _bidderOf[tokenId] != address(0) && block.timestamp <= (_bidTimeOf[tokenId] + AUCTION_WINDOW);
+    }
+
+    /// @notice returns if the current bidder can claim the handle
+    function canClaim(string memory handle) external view returns (bool) {
+        uint256 id = toTokenId(handle);
+        return _canClaim(id);
+    }
+
+    /// @dev internal to check if current auction is still open
+    function _canClaim(uint256 tokenId) internal view returns (bool) {
+        return _bidderOf[tokenId] != address(0) && block.timestamp > (_bidTimeOf[tokenId] + AUCTION_WINDOW) && block.timestamp <= (_bidTimeOf[tokenId] + AUCTION_WINDOW + CLAIM_WINDOW);
+    }
+
+    /// @notice deposit more stake into an handle
+    function deposit(string memory handle) external payable {
+        deposit(handle, msg.value, address(0));
+    }
+
+    /// @notice deposit more stake into an handle, with frontend fees
+    function deposit(string memory handle, uint256 amount, address frontend) public payable {
+        uint256 id = toTokenId(handle);
+        if (_ownerOf(id) == address(0)) revert HandleNotRegistered(handle);
+
+        _deposit(id, amount, frontend);
+    }
+
+    /// @dev internal deposit logic to reuse in external operations
+    function _deposit(uint256 id, uint256 amount, address frontend) internal {
+        if (msg.value == 0 || amount == 0) revert EthAmountRequired();
+        if (msg.value < amount) revert InvalidEthAmount(amount, msg.value);
+        if (frontend == address(0) && msg.value != amount) revert InvalidFrontendAddress();
+
+        _stakeOf[id] += amount;
+        // TODO: check if deposit covers the current bid, and return bid
+
+        // Send frontend fee if any
+        uint256 fee = msg.value - amount;
+        if (fee > 0) {
+            (bool sent, ) = frontend.call{value: fee}("");
+            if (!sent) revert FailedToTransfer(frontend, fee);
+        }
+    }
+
+    function withdraw(address to, string memory handle, uint256 amount) external {
+        uint256 id = _requireOwner(handle);
+        uint256 stake = _stakeOf[id];
+        if (amount >= stake) revert InvalidEthAmount(stake, amount);
+        _withdraw(to, id, amount);
+    }
+
+    /// @dev internal withdraw logic to reuse in external operations
+    function _withdraw(address to, uint256 tokenId, uint256 amount) internal {
+        // TODO:  Check that there's no ongoing bids
+        // transfer and update stake
+        _stakeOf[tokenId] -= amount;
+        (bool sent, ) = to.call{value: amount}("");
+        if (!sent) revert FailedToTransfer(to, amount);
     }
 
     /// @notice unregister an handle by the owner only, and receive the staked amount
-    function burn(string memory handle) external {
+    function burn(address to, string memory handle) external {
         uint256 id = _requireOwner(handle);
-        // TODO:  Check that there's no bids
         delete _uriOf[id];
-        _withdraw(handle, id, _stakeOf[id]);
+        _withdraw(to, id, _stakeOf[id]);
         _burn(id);
     }
 
+    /// @notice check if a handle is available to mint
+    function isAvailable(string memory handle) external view returns (bool) {
+        return _ownerOf(toTokenId(handle)) == address(0);
+    }
 
     /// @notice resolve a handle to the profile uri
     function resolve(string memory handle) external view returns (string memory) {
@@ -110,7 +207,7 @@ contract DHandle is SoulBoundERC721 {
 
     /// @notice return the profile uri of token
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        _requireOwned(tokenId);
+        _requireOwned(tokenId);     // from ERC721 implementation
         return _uriOf[tokenId];
     }
 
@@ -137,7 +234,7 @@ contract DHandle is SoulBoundERC721 {
         return true;
     }
 
-    /// @dev convert from handle string to bytes32
+    /// @dev internal convertion from handle string to bytes32
     function toBytes32(string memory handle) pure internal returns (bytes32 result) {
         bytes memory temp = bytes(handle);
         if (temp.length == 0 || temp.length > 32) return 0;
@@ -146,6 +243,7 @@ contract DHandle is SoulBoundERC721 {
         }
     }
 
+    /// @dev internal requires handle owner or otherwise reverts
     function _requireOwner(string memory handle) view internal returns (uint256 tokenId) {
         tokenId = toTokenId(handle);
         if (_ownerOf(tokenId) != address(msg.sender)) revert NotHandleOwner(handle);
